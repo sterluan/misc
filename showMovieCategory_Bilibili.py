ff2636eb4c7b3b96fce69d0152c40eff6e0d4b9e
Hao <luanster@gmail.com>
2015-06-28 10:59:09 +0800

    second commit

--- showMovieCategory_Bilibili.py
+++ showMovieCategory_Bilibili.py
@@ -18,18 +18,11 @@ fp = open('bilibili_movies.txt', 'w+')
 num_of_pages = 5
 for i in xrange(1, num_of_pages + 1):
     newurl = url1 + str(i) + url2
-    #print "newurl:", newurl
-    #raw_input()
     htmlfile = urllib2.urlopen(newurl)
     soup = BeautifulSoup(htmlfile)
     links = soup.findAll('a', attrs={"class":"preview"})
     for link in links:
         fullurl = urlparse.urljoin("http://" + urlparse.urlparse(url).hostname, link['href'])
-        #print isinstance(link['title'], str)
-        #print isinstance(fullurl, str)
-        #print isinstance(link['href'], str)
-        #print link['title'] + ": " + fullurl
         fp.write(link['title'] + ": " + fullurl + "\n")
-        #fp.write(fullurl + "\n")
         
 fp.close()
